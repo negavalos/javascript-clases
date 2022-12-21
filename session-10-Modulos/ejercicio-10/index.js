b43de9142d23e8a0a87b2e9ff99ae7349ff78c80
Nelson Avalos <negavalos99@gmail.com>
2022-12-21 15:54:26 -0300

    Add existing file

--- session-10-Modulos/ejercicio-10/index.js
+++
@@ -1,9 +0,0 @@
-import { suma, multiplica } from "./controller.js";
-import chalk from 'chalk';
-
-const suma1 = suma(1 , 2)
-const suma2 = suma(4, 5)
-
-const multiplicacion = multiplica(suma1, suma2)
-console.log(chalk.green(multiplicacion));
-
